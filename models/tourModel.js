64940b24baf7eeb217c389fed9f73e902606286c
poldevy <rostyslavshyian@gmail.com>
2025-01-08 14:36:55 +0100

    finished 93

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,49 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: [true, 'Duplicated name'],
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A Tour mast have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A Tour should have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: [true, 'A Tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have image cover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tours', tourSchema);
