1db40f15b20347b705996700f42594031d96b64f
poldevy <rostyslavshyian@gmail.com>
2025-01-08 13:36:53 +0100

    finished 92

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
@@ -107,17 +103,24 @@ exports.updatedTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  const updatedTours = tours.filter((item) => +item.id !== req.tour.id);
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
 
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(updatedTours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: null,
+    if (!tour) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Tour not found',
       });
     }
-  );
+    res.status(200).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
