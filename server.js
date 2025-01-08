722715ec4a7dc4df40d5e76fc60726b7b53d5a41
poldevy <rostyslavshyian@gmail.com>
2025-01-08 10:16:35 +0100

    finished 83

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
+const mongoose = require('mongoose');
 const app = require('./app');
-
 require('dotenv').config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful! 🪿🪿🪿'))
+  .catch((err) => console.log('💥💥💥💥', err));
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}...`);
