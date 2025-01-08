72966882715ddabb34881564dcb6af40f3677181
poldevy <rostyslavshyian@gmail.com>
2025-01-08 10:31:48 +0100

    finished 85

--- server.js
+++ server.js
@@ -16,6 +16,24 @@ mongoose
   .then(() => console.log('DB connection successful! 🪿🪿🪿'))
   .catch((err) => console.log('💥💥💥💥', err));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    uniq: [true, 'Duplicated name'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tout', tourSchema);
+
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}...`);
 });
