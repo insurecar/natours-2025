61929c3b75fed82eeb76c1e167f79730b30f91c0
poldevy <rostyslavshyian@gmail.com>
2025-01-14 13:59:51 +0100

    finished 95

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,7 +27,18 @@ exports.checkBody = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = structuredClone(req.query);
+    const excludedFields = ['page', 'sort', 'limit', 'fields']; //excluded fields for queries
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const tours = await Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals('5')
+    //   .where('difficulty')
+    //   .equals('easy');
+
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const app = require('./app');
 require('dotenv').config({ path: './config.env' });
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
