3368784fefd5d434cd3c089238c3aa88122535e9
poldevy <rostyslavshyian@gmail.com>
2025-01-15 14:27:38 +0100

    finished 97 and added else

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -45,6 +45,8 @@ exports.getAllTours = async (req, res) => {
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query.sort(sortBy);
+    } else {
+      query.sort('-createdAt');
     }
 
     const tours = await await query;
