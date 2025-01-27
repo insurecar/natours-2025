febb2f350e5bf37373fa4446a4db1c3778d971a4
poldevy <rostyslavshyian@gmail.com>
2025-01-27 14:03:36 +0100

    finished 103

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,7 @@ const {
   checkBody,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router(); //need to specify router
@@ -16,6 +17,7 @@ const router = express.Router(); //need to specify router
 // router.param('id', checkID); //check specific param
 
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
