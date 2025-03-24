1b0fa40a52707420f855384f0b331e32e40a6d78
poldevy <rostyslavshyian@gmail.com>
2025-03-24 15:49:57 +0100

    finished 119

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan'); // need for using middleware to see the time in terminal
 const tourRouter = require('./routes/tourRoutes');
 const gdUserRouter = require('./routes/gdUserRoutes');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -30,15 +31,7 @@ app.all('*', (req, res, next) => {
   next(err);
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 //4) Started SERVER
 module.exports = app;
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ID ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -7,18 +14,30 @@ const sendErrorDev = (err, res) => {
   });
 };
 
-const sendErrorProd = (err, res) => {
-  //Opertional, trusted error: send message to the client
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorProduction = (err, res) => {
+  //Operational, trusted error: send message to client
   if (err.isOperational) {
-    console.log('OKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOK');
     res.status(err.statusCode).json({
-      error: err,
+      status: err.status,
       message: err.message,
     });
-    //Programing or other error" don't leak error details
+    //Programming or other error: don't leak error details
   } else {
     //1) Log error
-    console.error('ERROR ⁉️⁉️⁉️⁉️');
+    console.error('Error 🍓🧨', err);
 
     //2) Send generic message
     res.status(500).json({
@@ -35,6 +54,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProduction(error, res);
   }
 };
