b586360139aef11781e8fd348c9f7178f2f31404
poldevy <rostyslavshyian@gmail.com>
2025-03-27 16:13:38 +0100

    finished 129

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,28 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  console.log(111);
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    paswordConfirm: req.body.paswordConfirm,
+  });
+
+  const token = jwt.sign(
+    {
+      id: newUser._id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    }
+  );
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
