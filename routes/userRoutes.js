ac66d2e5e846c45d03db60f04407c53b0c468792
poldevy <rostyslavshyian@gmail.com>
2025-03-31 14:55:06 +0200

    finished 130

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { signup } = require('./../controllers/authController');
+const { login } = require('./../controllers/authController');
+const { getAllUsers } = require('./../controllers/userController');
 
 router.post('/signup', signup);
+router.post('/login', login);
+router.get('/all', getAllUsers);
 
 module.exports = router;
